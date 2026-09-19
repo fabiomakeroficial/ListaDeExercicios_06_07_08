c943fec3390d47d6f934918170a3698fec148899
fabiomakeroficial <fabiomakeroficial@gmail.com>
2026-09-19 19:18:47 -0300

    Criando uma categoria nova (DVD), sem alterar nenhum método já escrito

--- src/Exercicio_01/config/ConfigBiblioteca.java
+++ src/Exercicio_01/config/ConfigBiblioteca.java
@@ -8,10 +8,12 @@ public class ConfigBiblioteca {
     // Regra dos prazos
     public static final int PRAZO_LIVRO = 14;
     public static final int PRAZO_REVISTA = 7;
+    public static final int PRAZO_DVD = 3;
 
     // Regras das multas
     public static final double MULTA_LIVRO = 0.5;
     public static final double MULTA_REVISTA = 1.0;
+    public static final double MULTA_DVD = 2.50;
 
     // Regras dos limites de emprestimos
     public static final int LIMITE_ALUNO = 3;
